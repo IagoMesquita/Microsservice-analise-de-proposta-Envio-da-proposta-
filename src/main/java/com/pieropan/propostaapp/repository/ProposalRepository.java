cbb7d9b592a92af6a01b0dc3cd5e912a1920df1f
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-18 10:32:08 -0300

    feat: add repository proposal

---
+++ src/main/java/com/pieropan/propostaapp/repository/ProposalRepository.java
@@ -0,0 +1,8 @@
+package com.pieropan.propostaapp.repository;
+
+import com.pieropan.propostaapp.entity.Proposal;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProposalRepository extends CrudRepository<Proposal, Long> {
+
+}
