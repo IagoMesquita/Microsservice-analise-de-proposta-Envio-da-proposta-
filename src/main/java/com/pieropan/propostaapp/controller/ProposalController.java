2faa683e414074d70a363485e49ff2f987529ecc
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-18 17:11:52 -0300

    feat: add controller get all proposals

--- src/main/java/com/pieropan/propostaapp/controller/ProposalController.java
+++ src/main/java/com/pieropan/propostaapp/controller/ProposalController.java
@@ -3,10 +3,12 @@ package com.pieropan.propostaapp.controller;
 import com.pieropan.propostaapp.dto.ProposalRequestDto;
 import com.pieropan.propostaapp.dto.ProposalResponseDto;
 import com.pieropan.propostaapp.service.ProposalService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,4 +39,19 @@ public class ProposalController {
     ).body(response);
   }
 
+  @GetMapping
+  public ResponseEntity<List<ProposalResponseDto>> getAllProposal() {
+    return ResponseEntity.ok().body(proposalService.getAll());
+  }
+
+
 }
+
+//Codigo para adcionar Location no retorno do Header. Boa pratica da Rest:
+
+/*
+ ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(response.getId())
+            .toUri()
+* */
