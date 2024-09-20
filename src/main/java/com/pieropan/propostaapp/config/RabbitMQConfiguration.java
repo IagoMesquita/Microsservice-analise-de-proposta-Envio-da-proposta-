0b0cac13afe6894e11c6a2ecbec6bb0d32d31ba2
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-20 17:21:03 -0300

    feat: add exchange and binds

--- src/main/java/com/pieropan/propostaapp/config/RabbitMQConfiguration.java
+++ src/main/java/com/pieropan/propostaapp/config/RabbitMQConfiguration.java
@@ -1,5 +1,9 @@
 package com.pieropan.propostaapp.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -11,23 +15,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
-
+  // Filas
+  @Bean
   public Queue createQueuePropostaPendenteMsAnaliseCredito() {
     return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
   }
 
+  @Bean
   public Queue createQueuePropostaPendenteMsNotificacao() {
     return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
   }
 
+  @Bean
   public Queue createQueuePropostaConcluidaMsProposta() {
     return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
   }
 
+  @Bean
   public Queue createQueuePropostaConcluidaMsNotificacao() {
     return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
   }
 
+  // Admin Filas, Exchanges
   @Bean
   public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory) {
     return new RabbitAdmin(connectionFactory);
@@ -38,26 +47,24 @@ public class RabbitMQConfiguration {
     return event -> rabbitAdmin.initialize();
   }
 
+  // Exchange Proposta e Binds para Queue de proposta-pendente e notificacao
+  @Bean
+  public FanoutExchange createFanoutExchangePropostaPendente() {
+    return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+  }
+
+  @Bean
+  public Binding createBindingPropostaPendenteMSAnaliseCredito() {
+    return BindingBuilder.bind(createQueuePropostaPendenteMsAnaliseCredito()).
+        to(createFanoutExchangePropostaPendente());
+  }
+
+  @Bean
+  public Binding createBindingPropostaPendenteToMSNotificacao() {
+    return BindingBuilder.bind(createQueuePropostaPendenteMsNotificacao()).
+        to(createFanoutExchangePropostaPendente());
+  }
 }
 
-// QueueBuilder para criar quatro filas (queues) duráveis no RabbitMQ.
-// As filas duráveis são persistentes, ou seja, elas não são perdidas se o RabbitMQ for reiniciado.
-
-//Os dois novos métodos adicionados à configuração do RabbitMQ têm o objetivo de garantir que as
-// filas sejam corretamente declaradas no RabbitMQ assim que a aplicação estiver pronta para uso.
-//
-//createRabbitAdmin:
-//
-//Esse método cria um bean de RabbitAdmin, que é uma classe do Spring AMQP responsável por gerenciar
-// automaticamente as declarações de filas, exchanges e bindings (ligações) no RabbitMQ.
-// O RabbitAdmin usa a ConnectionFactory (fábrica de conexões) para se conectar ao RabbitMQ e
-// realizar essas operações administrativas.
-//Resumindo, ele é o administrador das filas e outras configurações no RabbitMQ. Sem ele, as filas
-// que você definiu com QueueBuilder não seriam automaticamente declaradas no broker RabbitMQ.
-//
-// initializeAdmin:
-//
-//Esse método cria um listener que é disparado quando a aplicação está pronta para ser executada (evento ApplicationReadyEvent).
-// Quando esse evento ocorre, o listener chama o método rabbitAdmin.initialize(),
-// que força a inicialização do RabbitAdmin, garantindo que todas as filas configuradas sejam declaradas no RabbitMQ.
-//O objetivo é garantir que o RabbitAdmin inicialize no momento correto, após a aplicação estar completamente preparada, evitando erros ou problemas relacionados à conexão com o broker RabbitMQ.
+//Explicando os metodos com chatGPT:
+//https://chatgpt.com/share/66edd819-2098-800a-b73d-3328edac4d1a
