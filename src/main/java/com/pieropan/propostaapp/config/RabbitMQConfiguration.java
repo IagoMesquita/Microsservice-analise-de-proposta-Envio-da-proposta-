cafd2a1e802d2e4f2deee2f814a29b21c20db583
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-21 10:23:59 -0300

    refactor: add variable string in properties

--- src/main/java/com/pieropan/propostaapp/config/RabbitMQConfiguration.java
+++ src/main/java/com/pieropan/propostaapp/config/RabbitMQConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+
+  @Value("${rabbitmq.pending-proposal.exchange}")
+  private String exchangePendingProprosal;
+
   // Filas
   @Bean
   public Queue createQueuePropostaPendenteMsAnaliseCredito() {
@@ -53,7 +58,7 @@ public class RabbitMQConfiguration {
   // Exchange Proposta e Binds para Queue de proposta-pendente e notificacao
   @Bean
   public FanoutExchange createFanoutExchangePropostaPendente() {
-    return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+    return ExchangeBuilder.fanoutExchange(exchangePendingProprosal).build();
   }
 
   @Bean
