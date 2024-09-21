0f87d9218b2cb1ac446b445da0404f395d5b9962
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-21 16:54:31 -0300

    feat:add class with method that takes non-integrated records and integrates them in a scheduled manner

---
+++ src/main/java/com/pieropan/propostaapp/scheduler/ProposalsWithoutRabbitMQIntegration.java
@@ -0,0 +1,42 @@
+package com.pieropan.propostaapp.scheduler;
+
+import com.pieropan.propostaapp.repository.ProposalRepository;
+import com.pieropan.propostaapp.service.NotificationRabbitMQService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProposalsWithoutRabbitMQIntegration {
+
+  private final ProposalRepository proposalRepository;
+  private  final NotificationRabbitMQService notificationRabbitMQService;
+  private final String exchangePendingProposal;
+
+  private final Logger logger = LoggerFactory.getLogger(ProposalsWithoutRabbitMQIntegration.class);
+
+
+
+  public ProposalsWithoutRabbitMQIntegration(
+      ProposalRepository proposalRepository,
+      NotificationRabbitMQService notificationRabbitMQService,
+      @Value("${rabbitmq.pending-proposal.exchange}") String exchangePendingProposal) {
+    this.proposalRepository = proposalRepository;
+    this.notificationRabbitMQService = notificationRabbitMQService;
+    this.exchangePendingProposal = exchangePendingProposal;
+  }
+
+  public void searchProposalsWithoutIntegrationRabbitMQ() {
+    proposalRepository.findAllByIntegradaIsFalse().forEach(proposal -> {
+      try {
+        notificationRabbitMQService.notify(proposal, exchangePendingProposal);
+        proposal.setIntegrada(true);
+        proposalRepository.save(proposal);
+      }catch (RuntimeException exception) {
+        logger.error(exception.getMessage());
+      }
+    });
+  }
+
+}
