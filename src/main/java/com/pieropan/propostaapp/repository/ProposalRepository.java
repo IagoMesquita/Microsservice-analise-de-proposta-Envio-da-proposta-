5120838a9ec3d59b1cf2fdbf85ede08e28eee1db
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-21 16:52:42 -0300

    feat:add derived queries

--- src/main/java/com/pieropan/propostaapp/repository/ProposalRepository.java
+++ src/main/java/com/pieropan/propostaapp/repository/ProposalRepository.java
@@ -1,8 +1,10 @@
 package com.pieropan.propostaapp.repository;
 
 import com.pieropan.propostaapp.entity.Proposal;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProposalRepository extends CrudRepository<Proposal, Long> {
+  List<Proposal> findAllByIntegradaIsFalse();
 
 }
