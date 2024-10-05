1e94c8d9af67f610c3960e10dc92f027eb8c71b2
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-10-05 10:32:13 -0300

    feat: add Custom Query Method for better performance when updating the proposed table

--- src/main/java/com/iagomesquita/propostaapp/listener/ProposalCompletedListener.java
+++ src/main/java/com/iagomesquita/propostaapp/listener/ProposalCompletedListener.java
@@ -1,7 +1,10 @@
 package com.iagomesquita.propostaapp.listener;
 
+import com.iagomesquita.propostaapp.dto.ProposalResponseDto;
 import com.iagomesquita.propostaapp.entity.Proposal;
+import com.iagomesquita.propostaapp.mapper.ProposalMapper;
 import com.iagomesquita.propostaapp.repository.ProposalRepository;
+import com.iagomesquita.propostaapp.service.WebSocketService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +13,25 @@ public class ProposalCompletedListener {
 
   private final ProposalRepository proposalRepository;
 
-  public ProposalCompletedListener(ProposalRepository proposalRepository) {
+  private final WebSocketService webSocketService;
+
+  public ProposalCompletedListener(ProposalRepository proposalRepository,
+      WebSocketService webSocketService) {
     this.proposalRepository = proposalRepository;
+    this.webSocketService = webSocketService;
   }
 
   @RabbitListener(queues = "${rabbitmq.completed-proposal.queue}")
   public void proposalCompletedListener(Proposal proposal) {
     proposalRepository.save(proposal);
+    ProposalResponseDto proposalResponseDto = ProposalMapper.INSTANCE.convertProposalToDto(
+        proposal);
+    webSocketService.notify(proposalResponseDto);
+  }
+
+  private void updateProposal(Proposal proposal) {
+    proposalRepository.updateProposal(
+        proposal.getId(), proposal.getAprovada(), proposal.getObservacao()
+    );
   }
 }
--- src/main/java/com/iagomesquita/propostaapp/repository/ProposalRepository.java
+++ src/main/java/com/iagomesquita/propostaapp/repository/ProposalRepository.java
@@ -1,10 +1,19 @@
 package com.iagomesquita.propostaapp.repository;
 
 import com.iagomesquita.propostaapp.entity.Proposal;
+import jakarta.transaction.Transactional;
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProposalRepository extends CrudRepository<Proposal, Long> {
   List<Proposal> findAllByIntegradaIsFalse();
+
+  // Forma alternativa a usar diretamente o metodo save, que altera todos os dados
+  @Transactional
+  @Modifying
+  @Query(value = "UPDATE proposals SET aprovada = :isApproved, observacao = :observation, WHERE id = :id", nativeQuery = true)
+  void updateProposal(Long id, boolean isApproved, String observation);
 
 }
